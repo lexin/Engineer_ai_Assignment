046e7e3a820e9513fa024419ad7903f3f08bfef2
Oleksii Romanko <o.roman'ko@relevant.software>
2020-01-13 16:19:10 +0200

    initial commit

---
+++ Engineer_ai_Assignment/Engineer_ai_Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Engineer_ai_Assignment
+//
+//  Created by Alexey Romanko on 13.01.2020.
+//  Copyright © 2020 Romanko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
